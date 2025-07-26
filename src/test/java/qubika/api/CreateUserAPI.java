595dc32dd5f087e908a31e88f7c913cd418d64e5
StefanoMT20 <pieromezatejada14@hotmail.com>
2025-07-26 12:47:28 -0500

    refractor: function method changed

--- src/test/java/qubika/api/CreateUserAPI.java
+++ src/test/java/qubika/api/CreateUserAPI.java
@@ -13,7 +13,7 @@ public class CreateUserAPI {
 
     private static final String BASE_API_URL = "https://api.club-administration.qa.qubika.com";
 
-    public static void createUser() {
+    public static Response createUser() {
         UserData.email = "qubika_Piero_Meza_Test" + UUID.randomUUID() + "@mail.com";
         UserData.password = "TestPassword123!";
 
@@ -23,23 +23,11 @@ public class CreateUserAPI {
                 List.of("ROLE_ADMIN")
         );
 
-        Response response = given()
+        return given()
                 .baseUri(BASE_API_URL)
                 .header("Content-Type", "application/json")
                 .body(request)
                 .post("/api/auth/register");
-
-        int statusCode = response.statusCode();
-        String actualEmail = response.jsonPath().getString("email");
-        List<String> roles = response.jsonPath().getList("roles");
-
-        assert statusCode == 201 : "Expected status 201, but got: " + statusCode;
-        assert actualEmail.equals(UserData.email) : "Expected email: " + UserData.email + ", but got: " + actualEmail;
-        assert roles.contains("ROLE_ADMIN") : "Expected ROLE_ADMIN in roles, but got: " + roles;
-
-        System.out.println("Usuario creado correctamente:");
-        System.out.println("Email: " + actualEmail);
-        System.out.println("Roles: " + roles);
-        System.out.println("Status: " + statusCode);
     }
 }
+
